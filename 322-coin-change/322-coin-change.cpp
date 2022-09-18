d4b0c66cbf39cdef17847d9c012a6e7397fb5e95
Aman Sinha <54624963+amansinha6189@users.noreply.github.com>
2022-09-18 17:13:17 +0530

    Time: 245 ms (16.98%), Space: 40.8 MB (31.77%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,30 +3,33 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int N = coins.size();
         
-        vector<vector<int>> dp(N+1, vector<int> (amount+1, 0));
+        // vector<vector<int>> dp(N+1, vector<int> (amount+1, 0));
+        vector<int> prev(amount+1, 0);
         
-        for(int i = 0; i<=amount; i++){
-            dp[0][i] = INT_MAX - 1;
-        }
+        // for(int i = 0; i<=amount; i++){
+        //     dp[0][i] = INT_MAX - 1;
+        // }
         
         for(int i = 0; i<=amount; i++){
             if(i%coins[0] == 0){
-                dp[1][i] = i/coins[0];
+                prev[i] = i/coins[0];
             }else{
-                dp[1][i] = INT_MAX - 1;
+                prev[i] = INT_MAX - 1;
             }
         }
         
         for(int i = 2; i<=N; i++){
+            vector<int> curr(amount+1, 0);
             for(int j = 0; j<=amount; j++){
                 if(coins[i-1] > j){
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }else{
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i-1]] + 1); 
+                    curr[j] = min(prev[j], curr[j-coins[i-1]] + 1); 
                 }
             }
+            prev = curr;
         }
         
-        return dp[N][amount] < 1e6  ? dp[N][amount] : -1;
+        return prev[amount] < 1e6  ? prev[amount] : -1;
     }
 };
