3c6aaf26b47f02988f65a24cd71b0832d234b305
Aman Sinha <54624963+amansinha6189@users.noreply.github.com>
2022-10-02 12:41:42 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (16.79%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,42 +1,43 @@
 class Solution {
-    private: 
-     int memo[101];
 public:
-    int numDecodings(string s) {
-        memset(memo,-1,sizeof(int)*101);
-        return fun(s,0);
-    }
-    int fun(string s,int index)
-    {
-        // Base Case
-        if(index==s.size())
+    
+    int solve(string s, int index, vector<int> &dp){
+        if(index == s.length()){
             return 1;
+        }
         
-        // Using Memoization
-        if(memo[index]!=-1)
-            return memo[index];
-
-        int ans=0;
+        // if(s[index] == '0'){
+        //     return 0;
+        // }
         
-        // Recursive Call
-           // You have two choices 
-                 // 1. Choose one digit
-                // 2. Choose two digit
-         
-        // 1. Choose one digit
-            if(s[index]!='0')
-                ans+=fun(s,index+1);
+        if(dp[index] != -1){
+            return dp[index];
+        }
         
-        // 2. Choose two digit
-            if(index+1<s.size())
-            if(s[index]!='0')
-            {
-                int num=s[index]-'0';
-                 num*=10;
-                num+=s[index+1]-'0';
-                if(num<=26)
-                    ans+=fun(s,index+2);
+        int ans = 0;
+        
+        if(s[index] != '0'){
+            ans += solve(s, index+1, dp);
+        }
+        
+        if(index + 1 < s.length()){
+            if(s[index] != '0'){
+                int num = s[index] - '0';
+                num *= 10;
+                num += s[index+1] - '0';
+                if(num <= 26){
+                    ans += solve(s, index+2, dp);
+                }
             }
-        return memo[index]=ans;
+        }
+        
+        return dp[index] = ans;
+    }
+    int numDecodings(string s) {
+        int N = s.length();
+        vector<int> dp(101, -1);
+        
+        return solve(s, 0, dp);
+        
     }
 };
